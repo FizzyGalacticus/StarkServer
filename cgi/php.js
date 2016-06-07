1cb91b780254fe3032c81f81bb3ade97b1e546e0
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-06-07 13:28:12 -0800

    Added support for post and get parameters.

--- cgi/php.js
+++ cgi/php.js
@@ -1,5 +1,17 @@
 var exec = require('child_process').exec;
 
+var formatParams = function(params) {
+    var retStr = '';
+    for(var key in params) {
+        if(retStr.length > 0)
+            retStr += '&';
+
+        retStr += (key + '=' + params[key]);
+    }
+
+    return retStr;
+};
+
 PHPCGI   = function() {}
 
 PHPCGI.prototype.onGet = function(file, params, callback) {
@@ -11,8 +23,21 @@ PHPCGI.prototype.onPost = function(file, params, callback) {
 };
 
 PHPCGI.prototype.execute = function(file, callback, getParams, postParams) {
-    var cmd  = 'php ' + file;
-    var self = this;
+    var self             = this;
+    var getParamsString  = formatParams(getParams);
+    var postParamsString = formatParams(postParams);
+    var cmd              = '';
+
+    if(getParamsString.length > 0) {
+        cmd  = 'export SIMPLE_SERVER_QUERY_STRING="' + getParamsString + '";';
+        cmd += 'php -e -r \'parse_str($_SERVER["SIMPLE_SERVER_QUERY_STRING"], $_GET); include "' + file + '";\'';
+    }
+    else if(postParamsString.length > 0) {
+        cmd  = 'export SIMPLE_SERVER_QUERY_STRING="' + postParamsString + '";';
+        cmd += 'php -r \'parse_str($_SERVER["SIMPLE_SERVER_QUERY_STRING"], $_POST); include "' + file + '";\'';
+    }
+    else cmd = 'php ' + file;
+
     exec(cmd, function(error, stdout, stderr) {
         callback(self.getMimeType(), stdout);
     });
