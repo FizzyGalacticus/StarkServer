6d7444a9be188b049d5b1beed3f549ae13c27c64
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-08-25 15:42:03 -0800

    Hopefully fixed some posting issues as well as parameter issues.

--- cgi/php.js
+++ cgi/php.js
@@ -3,13 +3,19 @@ var exec = require('child_process').exec;
 var formatParams = function(params) {
     var retStr = '';
     for(var key in params) {
+        //Handle params where everything is in the key
+        if(key.length > 0 && params[key].length === 0)
+            retStr += formatParams(JSON.parse(key));
+
         if(retStr.length > 0)
             retStr += '&';
 
-        retStr += (key + '=' + params[key]);
+        var formattedParam = encodeURIComponent(params[key]);
+
+        retStr += (key + '=' + formattedParam);
     }
 
-    return retStr;
+    return retStr.replace(/'/g, '\'\'');
 };
 
 PHPCGI   = function() {};
@@ -29,17 +35,20 @@ PHPCGI.prototype.execute = function(file, callback, getParams, postParams) {
     var cmd              = '';
 
     if(getParamsString.length > 0) {
-        cmd  = 'export SIMPLE_SERVER_QUERY_STRING="' + getParamsString + '";';
+        cmd  = 'export SIMPLE_SERVER_QUERY_STRING=\'' + getParamsString + '\';';
         cmd += 'php -e -r \'parse_str($_SERVER["SIMPLE_SERVER_QUERY_STRING"], $_GET); include "' + file + '";\'';
     }
     else if(postParamsString.length > 0) {
-        cmd  = 'export SIMPLE_SERVER_QUERY_STRING="' + postParamsString + '";';
+        cmd  = 'export SIMPLE_SERVER_QUERY_STRING=\'' + postParamsString + '\';';
         cmd += 'php -r \'parse_str($_SERVER["SIMPLE_SERVER_QUERY_STRING"], $_POST); include "' + file + '";\'';
     }
     else cmd = 'php ' + file;
 
     exec(cmd, function(error, stdout, stderr) {
         callback(self.getMimeType(), stdout);
+        
+        if(stderr)
+            console.log(stderr);
     });
 };
 
